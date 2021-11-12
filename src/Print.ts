713066d177e82a01ff3942c52b97808b31c40b87
may h <mehrabhoque@gmail.com>
2021-11-12 02:04:32 -0500

    Align the printed values on their left sides
    e.g.:
    SUPER FRESH     1.49    Groceries       Wed Oct 27 2021
    DUNKIN #339369 Q35      1.73    Restaurant      Fri Nov 05 2021
    SUPER FRESH     2.39    Groceries       Wed Oct 28 2021
    KEY FOOD MARKET PLACE   2.17    Groceries       Wed Oct 20 2021
    RITE AID 10574  2.43    Convenience     Tue Oct 19 2021
    
    becomes:
    
    SUPER FRESH                             1.49            Groceries               Wed Oct 27 2021
    DUNKIN #339369 Q35                      1.73            Restaurant              Fri Nov 05 2021
    SUPER FRESH                             2.39            Groceries               Wed Oct 28 2021
    KEY FOOD MARKET PLACE                   2.17            Groceries               Wed Oct 20 2021
    RITE AID 10574                          2.43            Convenience             Tue Oct 19 2021

--- src/Print.ts
+++ src/Print.ts
@@ -30,16 +30,27 @@ export default function print(stats: txnsStats) {
   printSectionSeparator();
 
   printHeader('All Transactions');
-  printLabelAndValue('Number of transactions', stats.txns.length);
-  printLabelAndValue('Total Amount', stats.totalAmount);
-  printLabelAndValue('Average Amount', stats.averageAmount);
+  alignedPrint(
+    [
+      ['Number of transactions:', stats.txns.length.toString()],
+      ['Total Amount:', stats.totalAmount.toString()],
+      ['Average Amount:', stats.averageAmount.toString()],
+    ],
+    1,
+  );
   printBottomLevelSeparator();
 
   // TODO: This should also go to stderr so it can be re-routed, since it is
   // extra verbosity.
-  stats.txns.forEach((txn) => {
-    printTransactionAsRow(txn);
-  });
+  alignedPrint(
+    stats.txns.map((txn) => [
+      txn.description,
+      txn.amount.toString(),
+      txn.category,
+      dateToString(txn.transactionDate),
+    ]),
+    1,
+  );
   printSectionSeparator();
 }
 
@@ -51,24 +62,17 @@ function printMinMaxTransaction(stats: txnsStats) {
 }
 
 // Prints the transaction across lines, one for each field we care to print.
-// TODO: We should line up the values here on the same column for readability.
 function printTransactionAcrossLines(txn: Txn) {
-  printLabelAndValue('Description', txn.description);
-  // TODO: We should print this in a human friendly dollar format, e.g., $54.23.
-  // Meaning, we want the dollar sign, and want to always have 2 decimal places.
-  printLabelAndValue('Amount', txn.amount);
-  printLabelAndValue('Category', txn.category);
-  // TODO: We should only print the date (not time).
-  printLabelAndValue('Date', dateToString(txn.transactionDate));
-}
-
-// Prints the transaction as a row, with all fields we care to print on one
-// line.
-function printTransactionAsRow(txn: Txn) {
-  stdout.write(
-    `\t${txn.description}\t${txn.amount}\t${txn.category}\t${dateToString(
-      txn.transactionDate,
-    )}\n`,
+  alignedPrint(
+    [
+      ['Description:', txn.description],
+      // TODO: We should print this in a human friendly dollar format, e.g., $54.23.
+      // Meaning, we want the dollar sign, and want to always have 2 decimal places.
+      ['Amount:', txn.amount.toString()],
+      ['Category:', txn.category],
+      ['Date', dateToString(txn.transactionDate)],
+    ],
+    1,
   );
 }
 
@@ -83,16 +87,75 @@ function printCategoryStats(category: string, stats: categoryStats) {
   printBottomLevelSeparator();
   // TODO: This should go to stderr so it can be re-routed, since it is extra
   // verbosity.
-  stats.txns.forEach((txn) => {
-    printTransactionAsRow(txn);
-  });
-  printLabelAndValue('Total Amount', stats.totalAmount);
-  printLabelAndValue('Average Amount', stats.totalAmount);
+  alignedPrint(
+    stats.txns.map((txn) => [
+      txn.description,
+      txn.amount.toString(),
+      txn.category,
+      dateToString(txn.transactionDate),
+    ]),
+    1,
+  );
+  printBottomLevelSeparator();
+  // The alignment for this subsection should be a bit different.
+  alignedPrint(
+    [
+      ['Total Amount:', stats.totalAmount.toString()],
+      ['Average Amount:', stats.totalAmount.toString()],
+    ],
+    1,
+  );
 }
 
 ///
 /// Utility helper functions for the display format.
 ///
+
+// alignedPrint prints a 2D array of strings referring to multiple lines with
+// distinct pieces. It prints these lines such that for the nth line, the ith
+// piece lines up with the ith pieces of the (n-1)th line and (n+1)th line. The
+// alignment is on the _left_ (start) of each piece.
+// This function assumes that pieces contains arrays of all equal length. The
+// behavior is undefined if this is not true.
+//
+// For example:
+//
+// SUPER FRESH     1.49    Groceries       Wed Oct 27 2021
+// DUNKIN #339369 Q35      1.73    Restaurant      Fri Nov 05 2021
+// SUPER FRESH     2.39    Groceries       Wed Oct 28 2021
+// KEY FOOD MARKET PLACE   2.17    Groceries       Wed Oct 20 2021
+// RITE AID 10574  2.43    Convenience     Tue Oct 19 2021
+//
+// becomes:
+//
+// SUPER FRESH                             1.49            Groceries               Wed Oct 27 2021
+// DUNKIN #339369 Q35                      1.73            Restaurant              Fri Nov 05 2021
+// SUPER FRESH                             2.39            Groceries               Wed Oct 28 2021
+// KEY FOOD MARKET PLACE                   2.17            Groceries               Wed Oct 20 2021
+// RITE AID 10574                          2.43            Convenience             Tue Oct 19 2021
+function alignedPrint(lines: string[][], indent: number) {
+  const maxLengthValuePerColumn = Array(lines[0].length).fill(0);
+  // We iterate by _column_, so we want to loop for pieces[0].length, not
+  // pieces.length.
+  for (let i = 0; i < lines[0].length; i += 1) {
+    const ithColumnLengths = lines.map((pieces) => pieces[i].length);
+    maxLengthValuePerColumn[i] = Math.max(...ithColumnLengths);
+  }
+
+  // For each line, we want to print it out properly aligned and add a newline.
+  // For each piece in a line, we want to print the piece with enough spaces
+  // added so that every piece in the column has the same width, which ensures
+  // that the next pieces on each row start at the same position.
+  lines.forEach((pieces) => {
+    pieces.forEach((piece, i) => {
+      const paddedPiece = `${piece}${' '.repeat(
+        maxLengthValuePerColumn[i] - piece.length,
+      )}`;
+      stdout.write(`${'\t'.repeat(indent)}${paddedPiece}\t`);
+    });
+    stdout.write('\n');
+  });
+}
 
 // Prints a separator for use in separating sections.
 function printSectionSeparator() {
@@ -108,14 +171,4 @@ function printBottomLevelSeparator() {
 
 function printHeader(header: string) {
   stdout.write(`${header}:\n`);
-}
-
-interface StringableInterface {
-  toString(): string;
-}
-
-type Stringable = string | StringableInterface;
-
-function printLabelAndValue(label: string, value: Stringable) {
-  stdout.write(`\t${label}: ${value.toString()}\n`);
 }
