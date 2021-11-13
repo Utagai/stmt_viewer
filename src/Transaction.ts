6bf4b89ffeaea13ce73e60cd1afa55b52817427b
may h <mehrabhoque@gmail.com>
2021-11-13 13:32:05 -0500

    Clean up semantics & comments in src/Transaction.ts
    And just general code clean up.

--- src/Transaction.ts
+++ src/Transaction.ts
@@ -3,9 +3,15 @@ import parse from 'csv-parse/lib/sync';
 import { parse as dateparse } from 'date-fns';
 
 // This is a transaction that maps directly to the format of the CSV file we
-// read in. It is unprocessed and will eventually be converted to a
-// processedTransaction.
-type unprocessedTxn = {
+// read in. It is 'raw' and will eventually be converted to a proper Transaction
+// type.
+//
+// In contrast, a 'proper' transaction differs from an raw transaction
+// in a few ways:
+// * No spaces in the object keys (more ergonomic).
+// * Lowercases the field names for consistency.
+// * Gets rid of columns in the CSV we do not care about (e.g. Memo).
+type rawTransaction = {
   'Transaction Date': Date;
   'Post Date': Date;
   Description: string;
@@ -15,14 +21,7 @@ type unprocessedTxn = {
   Memo: string;
 };
 
-// A processed transaction differs from an unprocessed transaction in a few
-// ways:
-// * No spaces in the object keys (more ergonomic).
-// * Lowercases the field names for consistency.
-// * Gets rid of columns in the CSV we do not care about (e.g. Memo).
-//
-// It is otherwise the same thing.
-type processedTxn = {
+export type Transaction = {
   transactionDate: Date;
   description: string;
   category: string;
@@ -30,23 +29,19 @@ type processedTxn = {
   amount: number;
 };
 
-// Txn is a shorthand alias for a processed transaction. Consumers of this
-// module do not need to know about unprocessed vs. processed transactions.
-export type Transaction = processedTxn;
-
-function processTxn(unprocessed: unprocessedTxn): processedTxn {
+function convertFromRaw(rawTxn: rawTransaction): Transaction {
   return {
-    transactionDate: unprocessed['Transaction Date'],
-    description: unprocessed.Description,
-    category: unprocessed.Category,
-    type: unprocessed.Type,
-    amount: unprocessed.Amount,
+    transactionDate: rawTxn['Transaction Date'],
+    description: rawTxn.Description,
+    category: rawTxn.Category,
+    type: rawTxn.Type,
+    amount: rawTxn.Amount,
   };
 }
 
 export function parseTxns(csvFilepath: string): Transaction[] {
   const unparsedFileContents = readFileSync(csvFilepath, 'utf8');
-  const rows: unprocessedTxn[] = parse(unparsedFileContents, {
+  const rawTxns: rawTransaction[] = parse(unparsedFileContents, {
     cast: (value, context) => {
       // Unfortunately we have to do this for cases where we have descriptions
       // of the form (\w )+\d+. Not sure why, because if we don't csv-parse
@@ -71,5 +66,5 @@ export function parseTxns(csvFilepath: string): Transaction[] {
     skip_empty_lines: true,
   });
 
-  return rows.map((row) => processTxn(row));
+  return rawTxns.map((rawTxn) => convertFromRaw(rawTxn));
 }
